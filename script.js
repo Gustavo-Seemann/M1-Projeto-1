8f072e589d612d9986247eb8fb30c27c2bda3155
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-04-08 04:39:35 -0300

    criacao-adicionar-lista
    Criei a funcionalidade de adicionar a lista e remover item com o botão. Ainda falta ajustes.

---
+++ script.js
@@ -0,0 +1,74 @@
+const botaoAdd = document.getElementById('btnAdd')
+const botaoRemove = document.getElementById('btnRemove')
+const botaoLimpa = document.getElementById('btnLimpa')
+const nomeProduto = document.getElementById('nomeProd');
+const ul = document.getElementById('lista');
+
+var indice = 0;
+
+let listaUm = [];
+
+const listaJSON = localStorage.getItem('listaUm');
+
+
+if (listaJSON) {
+    listaUm = JSON.parse(listaJSON);
+    atualizaTela();
+}
+
+function retiraItem(id) {
+    const novaLista = [];
+    listaUm.forEach(function (item) {
+    if (item.id !== id) {
+        novaLista.push(item);
+    }
+    })
+    listaUm = novaLista;
+    atualizaTela();
+    salvaStorage();
+}
+
+function atualizaTela() {
+    ul.innerHTML = '';
+    listaUm.forEach(function (item) {
+    const btn = document.createElement('button');
+    btn.innerHTML = 'x';
+    btn.onclick = function () {
+        retiraItem(item.id);
+    }
+    const li = document.createElement('li');
+    li.id = item.id;
+    li.innerHTML = item.name;
+    li.appendChild(btn);
+    ul.appendChild(li);
+    });
+}
+
+function salvaStorage() {
+    const listaJSON = JSON.stringify(listaUm);
+    localStorage.setItem('listaUm', listaJSON);
+}
+
+function adicionaItem() {
+    if (nomeProduto.value) {
+        indice = indice + 1;
+        listaUm.push({
+            id: indice,
+            name: nomeProduto.value,
+            preco: 0
+    });
+    nomeProduto.value = '';
+    atualizaTela();
+    salvaStorage();
+    } else {
+        alert('Verifique o nome do produto!');
+    }
+}
+
+botaoAdd.addEventListener('click', adicionaItem);
+
+nomeProduto.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+    addItem();
+    }
+});
